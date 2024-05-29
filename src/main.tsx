3c65f365c3d5862e19887dbef9dafb65dc062047
Luclu7 <me@luclu7.fr>
2024-05-30 01:04:25 +0200

    passage à vite-react-ssg

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ViteReactSSG } from 'vite-react-ssg/single-page'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export const createRoot = ViteReactSSG(<App />)
